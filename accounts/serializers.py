0a8ef016f54a9f78813c0493a6b040249acd61dd
hassandn <hassandn6350@gmail.com>
2025-02-12 14:36:19 +0400

    clean code of serializers.py file

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,10 +1,17 @@
 from rest_framework import serializers
 from .models import User
 
-# سریالایزر برای مدل User
+
 class UserSerializer(serializers.ModelSerializer):
     profile_picture = serializers.ImageField(use_url=True, required=False)
 
     class Meta:
         model = User
-        fields = ['id', 'username', 'first_name', 'last_name', 'phone', 'profile_picture']
+        fields = [
+            "id",
+            "username",
+            "first_name",
+            "last_name",
+            "phone",
+            "profile_picture",
+        ]
