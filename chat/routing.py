76c224601c6c6ef5070762df9bb24fffef5b7cd9
hassandn <hassandn6350@gmail.com>
2025-02-12 14:09:14 +0400

    remove additional paths

--- chat/routing.py
+++ chat/routing.py
@@ -2,8 +2,6 @@ from django.urls import path
 from .consumers import ChatConsumer
 
 websocket_urlpatterns = [
-    # path(r'ws/chat/{pk}/$', ChatConsumer.as_asgi()),
-    # path(r'ws/chat/', ChatConsumer.as_asgi()),
     path('ws/chat/<int:chat_id>/',ChatConsumer.as_asgi()),
     path('ws/chat/', ChatConsumer.as_asgi()),
 ]
